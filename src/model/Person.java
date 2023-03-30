7e977f5998b2606fe274b4d38153e9bcdaa6b06f
Luis Yovany Romo <luis.romo@correounivalle.edu.co>
2023-03-30 16:38:01 -0500

    Change Person Class

--- src/model/Person.java
+++ src/model/Person.java
@@ -11,10 +11,12 @@ package model;
 public class Person {
     private int id;
     private String name;
+    private String lastName;
 
-    public Person(int id, String name) {
+    public Person(int id, String name, String lastName) {
         this.id = id;
         this.name = name;
+        this.lastName = lastName;
     }
 
     public int getId() {
@@ -33,6 +35,18 @@ public class Person {
         this.name = name;
     }
 
+    public String getLastName() {
+        return lastName;
+    }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+  
+
+    @Override
+    public String toString() {
+        return "Person{" + "id=" + id + ", name=" + name + '}';
+    }
     
 }
